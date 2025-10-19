4990198e3644f418a997bb9c04d11c79a7c964e7
1emank <dev.1emank@gmail.com>
2025-10-19 15:25:43 +0200

    Initial

---
+++ to_home.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+missing_deps=false
+exists() { 
+    if ! command -v "$1" >/dev/null 2>&1; then
+        echo The program "$1" is missing, you need to install it 1>&2
+        missing_deps=true
+    fi
+}
+
+exists git
+exists rsync
+exists realpath
+exists dirname
+
+$missing_deps && exit 1
+
+repo_dir="$(dirname "$( realpath "$0")" )"
+
+rsync "$repo_dir/sway" ~/.config --delete --recursive
+rsync "$repo_dir/waybar" ~/.config --delete --recursive
